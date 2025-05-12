bc8fd346e7ed4d9c7e2101125a972f4d46db73d4
Ana Rita <graminhaanarita@gmail.com>
2025-05-12 08:55:09 -0300

    Aula 10, 11 e 12

---
+++ src/DefaultPackage/aula10/Variaveis.java
@@ -0,0 +1,12 @@
+package DefaultPackage.aula10;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 19;
+        String nome= "Ana";
+        
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome); 
+
+    }
+}
---
+++ src/DefaultPackage/aula11/VariaveisChar.java
@@ -0,0 +1,12 @@
+package DefaultPackage.aula11;
+
+public class VariaveisChar {
+    public static void main(String[] args) {
+        
+        char o = 'o';
+        char i = 'i';
+        char interrogacao = 63;
+
+        System.out.println(""+o+i+interrogacao);
+    }
+}
