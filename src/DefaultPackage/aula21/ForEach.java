b34b41489a231f4720282c020f2bb3d1fc0ad840
Ana Rita <graminhaanarita@gmail.com>
2025-06-13 09:39:12 -0300

    For Each

---
+++ src/DefaultPackage/aula21/ForEach.java
@@ -0,0 +1,19 @@
+package DefaultPackage.aula21;
+
+import java.util.Random;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] notas = new int[10];
+
+        Random random = new Random();
+
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = random.nextInt(10);
+        }
+
+        for(int nota : notas){
+            System.out.println(nota);
+        }
+    }
+}
