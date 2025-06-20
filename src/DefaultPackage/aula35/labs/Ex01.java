409d1e7344e3b58b520401a9d896f5c89f01166f
Ana Rita <graminhaanarita@gmail.com>
2025-06-20 12:30:51 -0300

    Recursividade

---
+++ src/DefaultPackage/aula35/labs/Ex01.java
@@ -0,0 +1,12 @@
+package DefaultPackage.aula35.labs;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        
+        for (int i=0; i<20; i++){
+            System.out.print(Calculadora.fibonacci(i) + " ");
+        }
+        
+        
+    }
+}
---
+++ src/DefaultPackage/aula35/labs/Ex02.java
@@ -0,0 +1,8 @@
+package DefaultPackage.aula35.labs;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        
+        System.out.println(Calculadora.somatorio(5));
+    }
+}
