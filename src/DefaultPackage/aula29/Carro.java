700242e08b642362912f58c16fd0e86cd4327739
Ana Rita <graminhaanarita@gmail.com>
2025-06-20 12:18:26 -0300

    Construtores e Código de inicialização

---
+++ src/DefaultPackage/aula29/Carro.java
@@ -0,0 +1,17 @@
+package DefaultPackage.aula29;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+    
+    public Carro(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        this.capCombustivel = capCombustivel;
+        this.consumoCombustivel = consumoCombustivel;
+    }
+}
