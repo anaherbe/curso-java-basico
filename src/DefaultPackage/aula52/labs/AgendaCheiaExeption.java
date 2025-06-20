79492dc88e25ee42053d847d2b011a825d6aaf0b
Ana Rita <graminhaanarita@gmail.com>
2025-06-20 12:37:34 -0300

    Minha própria exceção

---
+++ src/DefaultPackage/aula52/labs/AgendaCheiaExeption.java
@@ -0,0 +1,10 @@
+package DefaultPackage.aula52.labs;
+
+public class AgendaCheiaExeption extends Exception {
+
+    @Override
+    public String getMessage() {
+       return "Agenda já está cheia";
+    }
+
+}
