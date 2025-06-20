9cab9528e8f9c89088eaff8ac9ff905133bb7fb2
Ana Rita <graminhaanarita@gmail.com>
2025-06-20 12:21:52 -0300

    Private e public

---
+++ src/DefaultPackage/aula31alt/TesteCarroAlt.java
@@ -0,0 +1,12 @@
+package DefaultPackage.aula31alt;
+
+import DefaultPackage.aula31.Carro;
+
+public class TesteCarroAlt {
+    public static void main(String[] args) {
+		
+		Carro carro = new Carro();
+		carro.marca = "Fiat";
+
+	}
+}
