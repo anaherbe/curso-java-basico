fd370550dfb83c762245029cb4cb5fcf616d2b64
Ana Rita <graminhaanarita@gmail.com>
2025-06-20 12:17:51 -0300

    Pacotes e import

---
+++ src/DefaultPackage/aula28/Pacotes.java
@@ -0,0 +1,18 @@
+package DefaultPackage.aula28;
+
+import DefaultPackage.aula17.labs.Ex06;
+import DefaultPackage.aula19.labs.Ex10;
+import DefaultPackage.aula20.labs.Ex05;
+import DefaultPackage.aula24.labs.Ex04;
+import DefaultPackage.aula27.Carro;
+
+public class Pacotes {
+    public static void main(String[] args) {
+        Carro carro;
+        Carro carro2;
+        Ex06 ex06;
+        Ex05 ex05;
+        Ex04 ex04;
+        Ex10 ex10;
+    }
+}
